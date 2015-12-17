d31781cad97f98cd7111fb3861d17ab062d1284e
mert <mertnesvat@gmail.com>
2015-12-17 17:36:22 +0200

    init.
    init.

---
+++ RPageControl/RPageControl.swift
@@ -0,0 +1,96 @@
+//
+//  RPageControl.swift
+//
+//
+//  Created by Mert Neşvat on 14/12/15.
+//  Copyright © 2015 ramistanbul. All rights reserved.
+//
+
+import UIKit
+
+struct Bullet {
+    
+    var index : Int = 0
+    var spacing : CGFloat = 5
+    var imageSize : CGSize = CGSizeMake(15, 15)
+    var activeDot : UIImage = UIImage()
+    var passiveDot : UIImage = UIImage()
+    var imageV : UIImageView = UIImageView()
+    var active : Bool = false {
+        didSet {
+            if active == true {
+                self.imageV.image = self.activeDot
+            }else{
+                self.imageV.image = self.passiveDot
+            }
+        }
+        
+    }
+    
+    init(isActive : Bool, withIndex i : Int, position : Int, gap : CGFloat, selectedBulletImageName : String, unSelectedBulletImageName : String){
+        
+        self.active = isActive
+        self.index = i
+        self.activeDot = UIImage(named: selectedBulletImageName)!
+        self.passiveDot = UIImage(named: unSelectedBulletImageName)!
+        self.spacing = gap
+        self.imageV  = UIImageView(frame: CGRect(x: CGFloat(position) * CGFloat(spacing+imageSize.width) , y: 0, width: imageSize.width, height: imageSize.height))
+
+        
+        
+    }
+}
+
+
+class RPageControl : UIView {
+    
+    
+    //meaurements and settings
+    var spacing : CGFloat = 5
+    var imageSize : CGSize = CGSizeMake(15, 15)
+    var activeImageName : String = "defaultActiveBullet"
+    var passiveImageName : String = "defaultPassiveBullet"
+    var pageBulletArray : [Bullet] = []
+    
+    var currentPage : Int = 0
+    var pageCount : Int = 0 {
+        didSet{
+            pageBulletArray.removeAll()
+            
+            var relativeIndex = pageCount/2
+            relativeIndex = -relativeIndex
+            for i in 0...pageCount{
+                
+//                let bullet = Bullet(isActive: false, withIndex: i, position: relativeIndex)
+                let bullet = Bullet(isActive: false, withIndex: i, position: relativeIndex, gap: spacing, selectedBulletImageName: activeImageName, unSelectedBulletImageName: passiveImageName)
+                pageBulletArray.append(bullet)
+                self.addSubview(bullet.imageV)
+                relativeIndex++
+            }
+            self.updateCurrentPageDisplay(withIndex: 0)
+            
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.pageCount = 3
+        
+    }
+    
+    
+    func updateCurrentPageDisplay(withIndex i: Int){
+        
+        for var b in self.pageBulletArray {
+            if i == b.index {
+                b.active = true
+            }else{
+                b.active = false
+            }
+        }
+        
+        
+    }
+    
+}
---
+++ RPageControl/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  RPageControl
+//
+//  Created by Mert Neşvat on 16/12/15.
+//  Copyright © 2015 Mert Neşvat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet var pageControl: [RPageControl]!
+    
+    var imageList = [UIImageView]()
+    let heroCount = 8
+    var imageWidth : CGFloat?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.setupUI()
+        
+    }
+    func setupUI(){
+        
+        imageWidth = scrollView.frame.height * 0.75
+        
+        
+        var co = 1
+        for pageC in pageControl {
+            pageC.activeImageName = "\(co)_pagingSelected"
+            pageC.passiveImageName = "\(co)_pagingUnselected"
+            pageC.spacing = CGFloat(4 + co)
+            pageC.imageSize = CGSize(width: 15+co, height: 15+co)
+            pageC.pageCount = heroCount
+            co++
+        }
+        
+        for index in 0...heroCount-1 {
+            
+            let imageV = UIImageView(image: UIImage(named: "\(index+1)"))
+            imageV.alpha = 0.7
+            
+            imageV.frame = CGRect(x: CGFloat(index) * imageWidth!, y: 0, width: imageWidth!, height: scrollView.frame.height)
+            scrollView.addSubview(imageV)
+            imageList.append(imageV)
+        }
+        
+        scrollView.contentSize = CGSize(width: CGFloat(imageList.count + 1) * imageWidth!, height: 0)
+        scrollView.contentOffset = CGPoint(x: 0, y:0)
+
+        
+
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if let a = imageWidth {
+            let index : Int = Int((scrollView.contentOffset.x + (self.view.frame.width/2)) / a)
+            
+            for pageC in pageControl {
+                pageC.updateCurrentPageDisplay(withIndex: index)
+            }
+            
+        
+            for i in 0...imageList.count-1 {
+                if i == index {
+                    imageList[i].alpha = 1
+                }else{
+                    imageList[i].alpha = 0.5
+                }
+            }
+            
+            
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
